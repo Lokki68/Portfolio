bd7af8da2c3fa23bc838ff623ccf787daf5c10f3
Brice <bricel67@gmail.com>
2022-06-05 12:35:32 +0200

    feat- header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,48 @@
 import React from "react";
 import Link from "next/link";
 
-import { DiCssdeck } from "react-icons/di";
+import { GiBookAura } from "react-icons/gi";
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
 
-import { Container } from "./HeaderStyles";
+import {
+  Container,
+  LogoContainer,
+  NavContainer,
+  NavLink,
+  SocialSection,
+} from "./HeaderStyles";
 
 const Header = () => (
   <Container>
-    <h1>Header</h1>
+    <LogoContainer>
+      <Link href="/">
+        <a className="logocontainer-link">
+          <GiBookAura size={30} />
+          <span>Portfolio</span>
+        </a>
+      </Link>
+    </LogoContainer>
+    <NavContainer>
+      <li>
+        <Link href="#projets">
+          <NavLink>Projets</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#technologies">
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#propos">
+          <NavLink>A propos</NavLink>
+        </Link>
+      </li>
+    </NavContainer>
+
+    <SocialSection>
+      <h2>Social</h2>
+    </SocialSection>
   </Container>
 );
 
--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -1,7 +1,5 @@
 import styled from "styled-components";
 
-import { IoIosArrowDown } from "react-icons/io";
-
 export const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
@@ -13,5 +11,69 @@ export const Container = styled.div`
     grid-template-rows: repeat(2, 60px);
     grid-column-gap: 0.5rem;
     grid-row-gap: 0.5rem;
+  }
+`;
+
+export const LogoContainer = styled.div`
+  //background-color: blue;
+  grid-area: 1 / 1 / 2 / 2;
+  display: flex;
+  flex-direction: row;
+  align-content: center;
+
+  .logocontainer-link {
+    display: flex;
+    align-items: center;
+    color: white;
+    margin-bottom: 20px;
+
+    span {
+      font-size: 2rem;
+      margin-left: 1rem;
+    }
+  }
+
+  @media ${(props) => props.theme.breakpoints.sm} {
+    grid-area: 1 / 1 / 2 / 4;
+  }
+`;
+
+export const NavContainer = styled.nav`
+  //background-color: red;
+  grid-area: 1 / 2 / 2 / 4;
+  display: flex;
+  justify-content: space-around;
+
+  @media ${(props) => props.theme.breakpoints.sm} {
+    grid-area: 2 / 1 / 3 / -1;
+  }
+`;
+
+export const NavLink = styled.a`
+  font-size: 1.5rem;
+  line-height: 32px;
+  color: rgba(255, 255, 255, 0.75);
+  cursor: pointer;
+  transition: 0.4s ease;
+
+  &:hover {
+    color: #f1f1f1;
+    opacity: 1;
+  }
+
+  @media ${(props) => props.theme.breakpoints.sm} {
+    padding: 0.5rem;
+  }
+`;
+
+export const SocialSection = styled.div`
+  //background-color: green;
+  grid-area: 1 / 5 / 2 / 6;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  @media ${(props) => props.theme.breakpoints.sm} {
+    grid-area: 1 / 4 / 2 / 6;
   }
 `;
