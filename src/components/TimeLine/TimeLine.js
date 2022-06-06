6c0af4628f4e4be70a75a5132d6801af12201068
Brice <bricel67@gmail.com>
2022-06-06 16:40:34 +0200

    Feat -timeline

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -1,7 +1,148 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { TimeLineData } from "../../constants/Constants";
+import {
+  CarouselButton,
+  CarouselButtonDot,
+  CarouselButtons,
+  CarouselContainer,
+  CarouselItem,
+  CarouselItemImg,
+  CarouselItemText,
+  CarouselItemTitle,
+  CarouselMobilScrollNode,
+} from "./TimeLineStyles";
+
+const TOTAL_CAROUSEL_COUNT = TimeLineData.length;
 
 function TimeLine(props) {
-  return <div>TimeLine</div>;
+  const [activeItem, setActiveItem] = useState(0);
+  const carouselRef = useRef();
+
+  const scroll = (node, left) => {
+    return node.scrollTo({ left, behavior: "smooth" });
+  };
+
+  const handleClick = (event, index) => {
+    event.preventDefault();
+
+    if (carouselRef.current) {
+      const scrollLeft = Math.floor(
+        carouselRef.current.scrollWidth * 0.7 * (index / TimeLineData.length)
+      );
+
+      scroll(carouselRef.current, scrollLeft);
+    }
+  };
+
+  const handleScroll = () => {
+    if (carouselRef.current) {
+      const index = Math.round(
+        (carouselRef.current.scrollLeft /
+          (carouselRef.current.scrollWidth * 0.7)) *
+          TimeLineData.length
+      );
+
+      setActiveItem(index);
+    }
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      scroll(carouselRef.current, 0);
+    };
+
+    window.addEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <Section id="propos">
+      <SectionTitle>A Propos</SectionTitle>
+      <SectionText>
+        41 ans, marié et père de trois enfants. Rigoureux, organisé et
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
+        dynamique, je suis doté d'un bon esprit d'initiative et d'une grande
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
+        facultée d'adaptation. J'aime travailler en équipe et relever de
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
+        nouveaux challenges. J'ai souhaité me reconvertir professionnellement,
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
+        afin d'allier ma passion à mon futur métier.
+      </SectionText>
+      <CarouselContainer ref={carouselRef} onScroll={handleScroll}>
+        <>
+          {TimeLineData.map((item, index) => (
+            <CarouselMobilScrollNode
+              key={index}
+              final={index === TOTAL_CAROUSEL_COUNT - 1}
+            >
+              <CarouselItem
+                index={index}
+                id={`carousel__item-${index}`}
+                active={activeItem}
+                onClick={(e) => handleClick(e, index)}
+              >
+                <CarouselItemTitle>
+                  {item.date}
+                  <CarouselItemImg
+                    width="208"
+                    height="6"
+                    viewBox="0 0 208 6"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M2.5 5.5C3.88071 5.5 5 4.38071 5 3V3.5L208 3.50002V2.50002L5 2.5V3C5 1.61929 3.88071 0.5 2.5 0.5C1.11929 0.5 0 1.61929 0 3C0 4.38071 1.11929 5.5 2.5 5.5Z"
+                      fill="url(#paint0_linear)"
+                      fillOpacity="0.33"
+                    />
+                    <defs>
+                      <linearGradient
+                        id="paint0_linear"
+                        x1="-4.30412e-10"
+                        y1="0.5"
+                        x2="208"
+                        y2="0.500295"
+                        gradientUnits="userSpaceOnUse"
+                      >
+                        <stop stopColor="white" />
+                        <stop
+                          offset="0.79478"
+                          stopColor="white"
+                          stopOpacity="0"
+                        />
+                      </linearGradient>
+                    </defs>
+                  </CarouselItemImg>
+                </CarouselItemTitle>
+                <CarouselItemText>{item.text}</CarouselItemText>
+              </CarouselItem>
+            </CarouselMobilScrollNode>
+          ))}
+        </>
+      </CarouselContainer>
+      <CarouselButtons>
+        {TimeLineData.map((item, index) => (
+          <CarouselButton
+            key={index}
+            index={index}
+            active={activeItem}
+            onClick={(e) => handleClick(e, index)}
+            type="button"
+          >
+            <CarouselButtonDot active={activeItem} />
+          </CarouselButton>
+        ))}
+      </CarouselButtons>
+    </Section>
+  );
 }
 
 export default TimeLine;
